86132ca55b22de46ffb5282b311e5005b3812272
Cinemato <alharerekhaled@gmail.com>
2022-09-16 23:17:45 +0300

    Finished responsive navigation menu

--- socialityproject/src/components/mobile-navigation/mobile-navigation.component.jsx
+++ socialityproject/src/components/mobile-navigation/mobile-navigation.component.jsx
@@ -1,14 +1,30 @@
 import './mobile-navigation.styles.css';
 
-import Logo from '../logo/logo.component';
+import { useState } from 'react';
+
 import { ReactComponent as MobileMenu } from '../../assets/mobile-menu.svg';
+import { ReactComponent as CloseMenu } from '../../assets/close-menu.svg';
+
+import Logo from '../logo/logo.component';
+import CompanySubBar from '../company-sub-bar/company-sub-bar.component';
+import ProfileSubBar from '../profile-sub-bar/profile-sub-bar.component';
 
 const MobileNavigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const mobileMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div className="mobile-navigation-container">
         <div className="mobile-navigation-info">
             <Logo className="mobile-logo" />
-            <MobileMenu />
+            {isMenuOpen ? <CloseMenu onClick={mobileMenuToggle} /> : <MobileMenu onClick={mobileMenuToggle} />}
+        </div>
+        <div className={`mobile-navigation ${isMenuOpen ? 'mobile-navigation-open' : ''}`}>
+              <CompanySubBar />
+              <ProfileSubBar />
         </div>
     </div>
   )
